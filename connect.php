a8791db95f2e98c91c8ba97c5282b9e3e2a71a4b
Raneem <83887615+raneemcodez@users.noreply.github.com>
2026-03-13 20:27:49 +0300

    Modify DSN and password in connect.php
    Updated database connection details for security.

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
-$dsn = "mysql:host=localhost;dbname=air_quality_db";
+$dsn = "mysql:host=localhost;dbname=........";
 $user = "root";
-$pass = "rrrRR345@#12";
+$pass = "";
 $option = array(
    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES UTF8"
 );
